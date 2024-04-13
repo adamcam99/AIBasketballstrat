e8763884396383623ac496cf42d8ce78560722ee
adamcam99 <W0474303@nscc.ca>
2024-04-13 12:23:02 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# import pyodbc
+
+# # Connect to your database
+# conn = pyodbc.connect('Driver={SQL Server};'
+#                       'Server=your_server_name;'
+#                       'Database=your_db_name;'
+#                       'Trusted_Connection=yes;')
+
+# cursor = conn.cursor()
+# cursor.execute('SELECT * FROM YourTable')
+# for row in cursor:
+#     print(row)
